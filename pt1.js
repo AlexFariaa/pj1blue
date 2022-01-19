8407c86210dfbb6ca31890ba744677c76a2fff1d
AlexFariaa <alexcaras1@hotmail.com>
2022-01-19 12:54:22 -0300

    Primeiro commit

---
+++ pt1.js
@@ -0,0 +1,28 @@
+console.clear();
+const pc = require('prompt-sync')();
+
+console.log(`Em um planeta não tão distante da terra, estavam acontecendo um verdadeiro caos.
+Jack Hannaford comandava seu batalhão para proteger a civilização dos goblins que ja destruiram outros continentes com muita facilidade.
+Mas dessa vez pode ser diferente! Você pode ajudar Jack a salvar essa nação e retomar forças para expurgar essas terriveis pragas da Centaurum 23-19.
+Você esta preparado?!`);
+console.log();
+
+let p1
+let p2
+let p3
+let p4
+let p5
+
+p1 = pc("Você conseguiu aprimorar seu equipamento de nivel 2 para o nivel 3 (max)? ");
+console.log();
+p2 = pc("Você conseguiu encontrar a Star Ship? ");
+console.log();
+p3 = pc("Você pegou a arma Vulcan? ");
+console.log();
+p4 = pc("Você esta com os sapatos voadores de Hannaford? ");
+console.log();
+p5 = pc("Você acertou em cheio a nave mãe dos goblins? ");
+console.log();
+
+
+
---
+++ pt2.js
@@ -0,0 +1,18 @@
+console.clear();
+const pc = require('prompt-sync')();
+
+console.log(`Em um planeta não tão distante da terra, estavam acontecendo um verdadeiro caos.
+Jack Hannaford comandava seu batalhão para proteger a civilização dos goblins que ja destruiram outros continentes com muita facilidade.
+Mas dessa vez pode ser diferente! Você pode ajudar Jack a salvar essa nação e retomar forças para expurgar essas terriveis pragas da Centaurum 23-19.
+Você esta preparado?!`);
+
+var p1
+var p2
+p1 = pc("Você conseguiu aprimorar seu equipamento de nivel 2 para o nivel 3 (max)? ");
+
+if (p1 == "sim"){
+    console.log("Legal, então você ja tem seu item de proteção e com o GPS embutido para encontrar a Star Ship");
+} else if (p2 == "nao"){
+    console.log("Não tem problema, temos chances de derrotar os goblins de outras maneiras");
+    
+}
